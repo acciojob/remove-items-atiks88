d2bdef920a12c66412696d86ae862440cb8e5e0a
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-10-29 18:31:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+function removeColor() {
+      var selectElement = document.getElementById('colorSelect');
+	 var selectedOption = selectElement.options[selectElement.selectedIndex];
+
+                selectElement.removeChild(selectedOption);
+            }
